f826f5d43269a2ccddc0dc5929ecd84d3903e08c
JunQi03 <hainiao05322@gmail.com>
2025-05-03 11:55:36 -0400

    final project

--- Assets/junqi_import/Scripts/Enemy roaming.cs
+++ Assets/junqi_import/Scripts/Enemy roaming.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemyroaming : MonoBehaviour
 {
+    public float speed = 5; 
+    public Transform[] patrolpoints;
+    private int patrolindex = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class Enemyroaming : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, patrolpoints[patrolindex].position, speed * Time.deltaTime);
+
+        if (transform.position == patrolpoints[patrolindex].position)
+        { 
+            patrolindex++;
+
+            if (patrolindex < patrolpoints.Length)
+            {
+
+            }
+
+        }
+
     }
 }
